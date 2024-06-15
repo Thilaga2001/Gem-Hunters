c9152f87bf2292ec041484c851db0ba8821ab39d
Thilaga2001 <thilagavedaraju@gmail.com>
2024-06-14 22:43:46 -0400

    Final Code commit
    Final Code commit

--- Gem Hunters/Gem Hunters/obj/Debug/net8.0/Gem Hunters.AssemblyInfo.cs
+++ Gem Hunters/Gem Hunters/obj/Debug/net8.0/Gem Hunters.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gem Hunters")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4843c03a2785089b590d26393c3a72f2def4b7b5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f41e728a8ac7b6067c51e3f47088200c6762d2c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gem Hunters")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gem Hunters")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
